32042323f2cbf9b12f7c9fd245cf00e4cbe751d4
shummi7 <sharmila16moorthi@gmail.com>
2023-03-13 14:54:32 -0500

    Updates

---
+++ precache-manifest.7a3921e2d7cbf442ebe069256659523b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "edd4aaa511c76469a219",
+    "url": "/face-detect/static/css/main.59538caf.chunk.css"
+  },
+  {
+    "revision": "edd4aaa511c76469a219",
+    "url": "/face-detect/static/js/main.edd4aaa5.chunk.js"
+  },
+  {
+    "revision": "ab2e0f225f6cfe4faade",
+    "url": "/face-detect/static/css/1.28025df8.chunk.css"
+  },
+  {
+    "revision": "ab2e0f225f6cfe4faade",
+    "url": "/face-detect/static/js/1.ab2e0f22.chunk.js"
+  },
+  {
+    "revision": "1ca9eadb57a5cee25dfb",
+    "url": "/face-detect/static/js/runtime~main.1ca9eadb.js"
+  },
+  {
+    "revision": "c81a95e93ef1403ccb9e6c696d7750b6",
+    "url": "/face-detect/static/media/brain.c81a95e9.png"
+  },
+  {
+    "revision": "593b6c68ae37284965ba1661e56ba91b",
+    "url": "/face-detect/index.html"
+  }
+];
